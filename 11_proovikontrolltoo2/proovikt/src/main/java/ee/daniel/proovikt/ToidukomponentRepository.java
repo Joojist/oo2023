8ef41a87ec36b32d61dbb07dbdf47136ce32aca0
Daniel Kupinski <kupinskidaniel+github@gmail.com>
2023-05-11 17:23:32 +0300

    Update

---
+++ 11_proovikontrolltoo2/proovikt/src/main/java/ee/daniel/proovikt/ToidukomponentRepository.java
@@ -0,0 +1,6 @@
+package ee.daniel.proovikt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToidukomponentRepository extends JpaRepository<Toidukomponent,Long> {
+}
---
+++ 11_proovikontrolltoo2/proovikt/src/main/java/ee/daniel/proovikt/ToitRepository.java
@@ -0,0 +1,6 @@
+package ee.daniel.proovikt;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ToitRepository extends JpaRepository<Toit,String> {
+}
